fcba40ad64367858203dce29dd10778c5b139f16
ygzh2008 <ygzh2008@qq.com>
2017-12-26 17:05:30 +0800

    tests

--- BeforeFieldInit/Program.cs
+++ BeforeFieldInit/Program.cs
@@ -10,6 +10,60 @@ namespace BeforeFieldInit
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("Main 开始 ...");
+            //Foo.GetString("手动调用 Foo.GetString() 方法!");
+            //string info = Foo.Field;
+
+            //FooStatic.GetString("手动调用 FooStatic.GetString() 方法！");
+            //string infoStatic = FooStatic.Field;
+            //Console.ReadKey();
+
+            Parallel.For(0, 100, i =>
+            {
+                for (int j = 0; j < 1000; j++)
+                {
+                    MySingleton.Instance.IncrementCounter();
+                }
+            });
+
+            Console.WriteLine($"Counter={MySingleton.Instance.Counter}");
+            Console.ReadKey();
+        }
+    }
+
+    internal class Foo
+    {
+        Foo()
+        {
+            Console.WriteLine("Foo 对象构造函数");
+        }
+
+        public static string Field = GetString("初始化 Foo 静态成员变量！");
+
+        public static string GetString(string s)
+        {
+            Console.WriteLine(s);
+            return s;
+        }
+    }
+
+    internal class FooStatic
+    {
+        static FooStatic()
+        {
+            Console.WriteLine("FooStatic 类构造函数");
+        }
+
+        FooStatic()
+        {
+            Console.WriteLine("FooStatic 对象构造函数");
+        }
+
+        public static string Field = GetString("初始化 FooStatic 静态成员变量！");
+        public static string GetString(string s)
+        {
+            Console.WriteLine(s);
+            return s;
         }
     }
 }
---
+++ BeforeFieldInit/Singleton.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace BeforeFieldInit
+{
+    public class SingletonLazy
+    {
+        #region Lazy Singleton
+        private static readonly Lazy<SingletonLazy> _instace = new Lazy<SingletonLazy>(() => new SingletonLazy(), LazyThreadSafetyMode.ExecutionAndPublication);
+
+        private SingletonLazy()
+        {
+
+        }
+
+        public static SingletonLazy Instance
+        {
+            get { return _instace.Value; }
+        } 
+        #endregion
+    }
+
+    public sealed class SingletonNest
+    {
+        private SingletonNest()
+        { }
+
+        public static SingletonNest Instance
+        {
+            get { return Nested._instance; }
+        }
+
+        private class Nested
+        {
+            static Nested()
+            {
+
+            }
+
+            internal static readonly SingletonNest _instance = new SingletonNest();
+        }
+    }
+
+    public class SingletonStatic
+    {
+        private static readonly SingletonStatic _instance = new SingletonStatic();
+        public static SingletonStatic Instance
+        {
+            get { return _instance; }
+        }
+
+        private SingletonStatic()
+        { }
+
+        static SingletonStatic()
+        { }
+    }
+
+    public sealed class SingletonLock
+    {
+        private static volatile SingletonLock instance = null;
+        private static object syncRoot = new object();
+
+        private SingletonLock()
+        { }
+
+        public static SingletonLock Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock(syncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new SingletonLock();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+    }
+
+    public sealed class SingletonPrivate
+    {
+        private static readonly SingletonPrivate instance = new SingletonPrivate();
+
+        private SingletonPrivate()
+        { }
+
+        public static SingletonPrivate Instance
+        {
+            get { return instance; }
+        }
+    }
+
+    public sealed class Singleton
+    {
+        private static Singleton instance;
+        private Singleton()
+        { }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+
+                return instance;
+            }
+        }
+    }
+
+    public abstract class SingletonGeneric<T>
+    {
+        private static readonly Lazy<T> _instance = new Lazy<T>(() =>
+        {
+            var ctors = typeof(T).GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+            if (ctors.Count() != 1)
+            {
+                throw new InvalidOperationException($"Type constructor for {typeof(T)} must be private and take no paraneters.");
+            }
+
+            var ctor = ctors.SingleOrDefault(c => c.GetParameters().Count() == 0 && c.IsPrivate);
+            if (ctor == null)
+                throw new InvalidOperationException($"The constructor for {typeof(T)} must be private and take no parameters.");
+            return (T)ctor.Invoke(null);
+        });
+
+        public static T Instance
+        {
+            get { return _instance.Value; }
+        }
+    }
+
+    class MySingleton : SingletonGeneric<MySingleton>
+    {
+        int _counter;
+        public int Counter
+        {
+            get { return _counter; }
+        }
+
+        private MySingleton()
+        {
+            _counter = 0;
+        }
+
+        public void IncrementCounter()
+        {
+            Interlocked.Increment(ref _counter);
+            //++_counter;
+        }
+    }
+}
